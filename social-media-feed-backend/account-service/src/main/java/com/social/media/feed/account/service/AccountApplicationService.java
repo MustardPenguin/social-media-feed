7056a3c3bdacb6666d633d32c1e0cd5b1256cbab
mustardpenguin <andyha234@gmail.com>
2024-04-23 21:43:45 -0400

    Added eureka service

--- social-media-feed-backend/account-service/src/main/java/com/social/media/feed/account/service/AccountApplicationService.java
+++ social-media-feed-backend/account-service/src/main/java/com/social/media/feed/account/service/AccountApplicationService.java
@@ -2,8 +2,10 @@ package com.social.media.feed.account.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountApplicationService {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplicationService.class, args);
