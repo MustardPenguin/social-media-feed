7b6dfab815476b9f05a372017c0244863298b6b2
mustardpenguin <andyha234@gmail.com>
2024-05-11 23:42:34 -0400

    Updated tests to use testcontainers

--- social-media-feed-backend/account-service/src/main/java/com/social/media/feed/account/service/application/rest/model/LoginAccountRequestBody.java
+++ social-media-feed-backend/account-service/src/main/java/com/social/media/feed/account/service/application/rest/model/LoginAccountRequestBody.java
@@ -1,10 +1,12 @@
 package com.social.media.feed.account.service.application.rest.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginAccountRequestBody {
 
     private String username;
--- social-media-feed-backend/account-service/src/test/java/com/social/media/feed/account/service/AccountApplicationServiceTest.java
+++ social-media-feed-backend/account-service/src/test/java/com/social/media/feed/account/service/AccountApplicationServiceTest.java
@@ -2,9 +2,14 @@ package com.social.media.feed.account.service;
 
 import com.social.media.feed.account.service.application.dto.AccountDetails;
 import com.social.media.feed.account.service.application.port.service.AuthenticationService;
+import com.social.media.feed.account.service.application.rest.controller.AuthenticateController;
+import com.social.media.feed.account.service.application.rest.model.LoginAccountRequestBody;
 import com.social.media.feed.account.service.application.util.JwtTokenUtil;
 import com.social.media.feed.account.service.domain.entity.Account;
 import io.jsonwebtoken.Claims;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,41 +18,53 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.MountableFile;
 
+import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class AccountApplicationServiceTest {
 
     @Autowired
-    private AuthenticationService authenticationService;
-    @Autowired
-    private UserDetailsService userDetailsService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+    private AuthenticateController authenticateController;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+            .withUrlParam("stringtype", "unspecified")
+            .withUrlParam("currentSchema", "account")
+            .withInitScript("init-schema.sql")
+            .withReuse(true);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
 
     @Test
-    public void testAuthentication() {
-        final Account account = Account.builder()
-                .accountId(UUID.randomUUID())
-                .username("test")
-                .password("test")
-                .build();
-        String encodedPassword = passwordEncoder.encode(account.getPassword());
-        when(userDetailsService.loadUserByUsername(account.getUsername())).thenReturn(
-                new AccountDetails(account.getId().getValue(), account.getUsername(), encodedPassword));
-
-        String token = authenticationService.authenticateAccount(account);
-        Claims claims = jwtTokenUtil.extractAllClaims(token);
-        String username = claims.getSubject();
-        String accountId = claims.get("accountId", String.class);
-
-        assertEquals(account.getUsername(), username);
-        assertEquals(account.getId().getValue().toString(), accountId);
+    public void testAuthentication()  {
+        LoginAccountRequestBody loginAccountRequestBody = new LoginAccountRequestBody("test", "test");
+        String token = authenticateController.authenticate(loginAccountRequestBody);
+        assertNotNull(token);
     }
 }
