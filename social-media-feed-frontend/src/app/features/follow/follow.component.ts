2d520221efbb40c879b12d6eb2d9c96d4f1a7945
mustardpenguin <andyha234@gmail.com>
2024-05-19 22:26:01 -0400

    Updated follow page

--- social-media-feed-frontend/src/app/features/follow/follow.component.ts
+++ social-media-feed-frontend/src/app/features/follow/follow.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { FormComponent } from '../../shared/form/form.component';
 import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormComponent } from '../../shared/form/form.component';
 import FieldType from '../../shared/interfaces/FieldType';
 import Follow from '../../shared/interfaces/Follow';
+import FormData from '../../shared/interfaces/FormData';
+import { FormService } from '../../core/form.service';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-follow',
   standalone: true,
-  imports: [FormsModule, FormComponent],
+  imports: [FormsModule, FormComponent, MatButtonModule],
   templateUrl: './follow.component.html',
   styleUrl: './follow.component.css'
 })
@@ -15,17 +22,26 @@ export class FollowComponent {
   fields: FieldType[] = [
     { name: "follow", type: "text", icon: "add_reaction", placeholder: 'username or id' },
   ];
-
-  followers: Follow[] = [
-    { accountId: '956c253b-0d4f-457e-a96d-31cce7639eaa', username: 'user1' },
-    { accountId: '956c253b-0d4f-457e-a96d-31cce7639eaa', username: 'user2' },
-    { accountId: '956c253b-0d4f-457e-a96d-31cce7639eaa', username: 'user3' }
-  ];
-  following: Follow[] = [
-    { accountId: '956c253b-0d4f-457e-a96d-31cce7639eaa', username: 'user4' },
-    { accountId: '956c253b-0d4f-457e-a96d-31cce7639eaa', username: 'user5' },
-    { accountId: '956c253b-0d4f-457e-a96d-31cce7639eaa', username: 'user6' }
-  ];
+  followers: Follow[] = [ { accountId: "", username: "No followers!" } ];
+  following: Follow[] = [ { accountId: "", username: "No follows!" } ];
+  private connection: Subscription | null = null;
 
 
+  constructor(private formService: FormService) { }
+
+  ngOnInit(): void {
+    this.connection = this.formService.getFormData().subscribe(formData => {
+      this.submitForm(formData);
+    });
+  }
+
+  async submitForm(formData: FormData): Promise<void> {
+    console.log(formData);
+  }
+
+  ngOnDestroy(): void {
+    if(this.connection !== null) {
+      this.connection.unsubscribe();
+    }
+  }
 }
