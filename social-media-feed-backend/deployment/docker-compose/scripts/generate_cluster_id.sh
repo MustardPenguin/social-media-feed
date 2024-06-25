1275626d2c80dd5a0decd951623b29d7eee14b9c
mustardpenguin <andyha234@gmail.com>
2024-06-24 23:45:15 -0400

    Added Kafka storage id generation

---
+++ social-media-feed-backend/deployment/docker-compose/scripts/generate_cluster_id.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Source: https://medium.com/@katyagorshkova/docker-compose-for-running-kafka-in-kraft-mode-20c535c48b1a
+# Github: https://github.com/katyagorshkova/kafka-kraft/tree/main
+
+file_path="/tmp/clusterID/clusterID"
+
+if [ ! -f "$file_path" ]; then
+  /bin/kafka-storage random-uuid > /tmp/clusterID/clusterID
+  echo "Cluster id has been  created..."
+fi
